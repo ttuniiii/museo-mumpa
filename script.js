45bac3371b103bd5c0e6903b2291a89fad4261c9
morecorbanini <morecorbanini2007@gmail.com>
2026-05-14 15:59:22 -0300

    Create script.js
    Co-Authored-By: oriannagrecco <284389149+oriannagrecco@users.noreply.github.com>
    Co-Authored-By: ttuniiii <188381312+ttuniiii@users.noreply.github.com>
    Co-Authored-By: julietaa valdez <274889107+valdez28juli-jpg@users.noreply.github.com>
    Co-Authored-By: rocioahilenbrizuela-ai <284393589+rocioahilenbrizuela-ai@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,7 @@
+const boton = document.getElementById("btnInfo");
+
+boton.addEventListener("click", () => {
+
+    alert("Muy pronto habrá nuevas exhibiciones interactivas de dinosaurios.");
+
+});
